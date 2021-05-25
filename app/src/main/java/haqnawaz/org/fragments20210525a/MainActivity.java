c6d4dda3223df2c9def5347641529f108ddd5dad
Haq Nawaz <haqnawaz99@yahoo.com>
2021-05-25 15:16:03 +0500

    Fragment 1 Added

--- app/src/main/java/haqnawaz/org/fragments20210525a/MainActivity.java
+++ app/src/main/java/haqnawaz/org/fragments20210525a/MainActivity.java
@@ -1,14 +1,37 @@
 package haqnawaz.org.fragments20210525a;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Button1, Button2, Button3;
+    LinearLayout myLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button1 = findViewById(R.id.btn_1);
+        Button2 = findViewById(R.id.btn_2);
+        Button3 = findViewById(R.id.btn_3);
+        myLayout = findViewById(R.id.linearLayout);
+
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment1 fragment1 = new Fragment1();
+                FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout, fragment1);
+                transaction.commit();
+            }
+        });
+
     }
 }
