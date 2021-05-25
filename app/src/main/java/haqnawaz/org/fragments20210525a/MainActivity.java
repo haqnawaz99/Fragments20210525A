25e77383ba2aa08bbd00c2e607699ef454a77e63
Haq Nawaz <haqnawaz99@yahoo.com>
2021-05-25 15:02:53 +0500

    Initial commit

---
+++ app/src/main/java/haqnawaz/org/fragments20210525a/MainActivity.java
@@ -0,0 +1,14 @@
+package haqnawaz.org.fragments20210525a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
